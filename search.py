3bcff42e0f4acfe676a449196213a171e0c7181d
Ken Hung <eiekenhung@gmail.com>
2020-12-29 20:41:07 +0800

    use ConceptNet word embeddings for similarity

--- search.py
+++ search.py
@@ -1,11 +1,13 @@
 import itertools
 import json
-import os
-from pathlib import Path
+from functools import lru_cache
 from typing import Dict, List, Optional, Tuple
 
 import nltk
+import numpy as np
+import pandas as pd
 from nltk.corpus import wordnet as wn
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def sentence_similarity(keywords_tokens, sentence_tokens):
@@ -13,7 +15,8 @@ def sentence_similarity(keywords_tokens, sentence_tokens):
     sentence_tokens = remove_stopwords(sentence_tokens)
     kw_score = {}
     for kw in keywords:
-        word_score = {tk.word: similarity(kw, tk) for tk in sentence_tokens}
+        word_score = {tk.word: similarity(kw, tk.word)
+                      for tk in sentence_tokens}
         if word_score:
             match_word = max(word_score, key=word_score.get)
             kw_score[match_word] = word_score[match_word]
@@ -23,69 +26,28 @@ def sentence_similarity(keywords_tokens, sentence_tokens):
 
 
 def remove_stopwords(tokens) -> list:
+    # TODO: return tk.word
     return [tk for tk in tokens if tk.flag != 'x']
 
 
-def similarity(token_a, token_b) -> float:
-    word_a, pos_a = map_pos(token_a)
-    word_b, pos_b = map_pos(token_b)
-
-    if word_a == word_b:
+@lru_cache(maxsize=None)
+def similarity(word1: str, word2: str) -> float:
+    if word1 == word2:
         return 1
 
-    ss_a = get_synsets(word_a, pos_a)
-    ss_b = get_synsets(word_b, pos_b)
-    if ss_a and ss_b:
-        sims = map(lambda p: wn.path_similarity(*p) or 0,
-                   itertools.product(ss_a, ss_b))
-    elif ss_a:
-        lemmas = (syn.lemma_names(lang='cmn') for syn in ss_a)
-        sims = [(1 if word_b == lemma else 0) for lemma in lemmas]
-    elif ss_b:
-        lemmas = (syn.lemma_names(lang='cmn') for syn in ss_b)
-        sims = [(1 if word_a == lemma else 0) for lemma in lemmas]
-    else:
-        sims = [0]
-    return max(sims)
+    try:
+        vec1 = _embeddings.loc[f'/c/zh/{word1}'].to_numpy()
+        vec2 = _embeddings.loc[f'/c/zh/{word2}'].to_numpy()
+        return cosine_similarity(vec1.reshape(1, -1), vec2.reshape(1, -1))[0][0]
+    except KeyError:
+        # TODO: try to find related terms and compare
+        return 0
 
 
-def get_synsets(word, pos):
-    ss = wn.synsets(word, pos=pos, lang='cmn')
-    if not ss:
-        ccn_words = _conceptnet_terms.get(word)
-        if ccn_words:
-            all_ss = (wn.synsets(ccn_word, pos=pos) for ccn_word in ccn_words)
-            ss = set(itertools.chain(*all_ss))
-    return ss
+_embeddings: pd.DataFrame = None
 
 
-def map_pos(token) -> Tuple[str, Optional[str]]:
-    if type(token) is str:
-        return token, None
-    else:
-        wn_pos = {
-            'n': wn.NOUN,
-            'v': wn.VERB,
-            'a': wn.ADJ,
-            'd': wn.ADV
-        }
-        pos = wn_pos.get(token.flag[0], token.flag)
-        return token.word, pos
-
-
-_conceptnet_terms: Dict[str, List[str]] = {}
-
-
-def load_conceptnet_terms(in_dir: str) -> None:
-    global _conceptnet_terms
-    for word_file in Path(in_dir).iterdir():
-        with open(word_file) as f:
-            ccn_resp = json.load(f)
-            en_labels = extract_en_labels(ccn_resp)
-            if en_labels:
-                _conceptnet_terms[word_file.stem] = en_labels
-
-
-def extract_en_labels(ccn_resp: dict) -> List[str]:
-    edges = ccn_resp.get('edges', [])
-    return [node['end']['label'] for node in edges if node['end'].get('language') == 'en']
+def load_word_embeddings(path: str) -> None:
+    global _embeddings
+    _embeddings = pd.read_hdf(path)
+    _embeddings = _embeddings[_embeddings.index.str.startswith('/c/zh')]
--- test_search.py
+++ test_search.py
@@ -1,4 +1,3 @@
-import jieba
 import jieba.posseg as pseg
 
 import search
@@ -9,10 +8,11 @@ def test_search():
     vers = '亚当生塞特；塞特生以挪士；'
     kw_tk = pseg.lcut(kw)
     vers_tk = pseg.lcut(vers)
+    search.load_word_embeddings('data/mini.h5')
     similarity = search.sentence_similarity(kw_tk, vers_tk)
     assert similarity is not None
 
 
 def test_conceptnet_similarity():
-    search.load_conceptnet_terms('data/conceptnet')
-    assert search.similarity('喜乐', '欢喜') > 0
+    search.load_word_embeddings('data/mini.h5')
+    assert search.similarity('欢乐', '欢欣') > 0
