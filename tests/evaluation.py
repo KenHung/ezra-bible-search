2f3c48ee2bcd84be5e4ed2c78a7de475660ac4af
Ken Hung <eiekenhung@gmail.com>
2021-01-10 09:12:22 +0800

    update word tokens

--- tests/evaluation.py
+++ tests/evaluation.py
@@ -10,13 +10,13 @@ from word_embeddings.conceptnet_strategy import ConceptNetStrategy
 strategy = ConceptNetStrategy()
 
 ezra_engine = BibleSearchEngine(strategy)
-keywords = open('keywords.txt').read().splitlines()
+keywords = open('word_tokens.txt').read().splitlines()
 keywords_s = map(HanziConv.toSimplified, keywords)
 kw_pairs = list(combinations(keywords_s, 2))
 
-for kw1, kw2 in tqdm(kw_pairs):
-    with open('evaluation.ans', 'a') as outfile:
-        with redirect_stdout(outfile):
+with open('evaluation.ans', 'w') as outfile:
+    with redirect_stdout(outfile):
+        for kw1, kw2 in tqdm(kw_pairs):
             try:
                 ezra_engine.search(f'{kw1} {kw2}', top_k=5, verbose=True)
             except Exception as ex:
